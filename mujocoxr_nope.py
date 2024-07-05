791c78e12e3f40dd0bec1205037f4295da40908a
SkytAsul <skytasul@gmail.com>
2024-07-05 10:14:32 +0200

    Damnit

--- mujocoxr_nope.py
+++ mujocoxr_nope.py
@@ -1,15 +1,40 @@
 """
 Standalone program which displays a MuJoCo scene on a VR device using openXR.
 """
+import mujoco.gl_context
+import mujoco.glfw
 import xr
-import mujoco
-from OpenGL import GL
 from ctypes import byref
+
+import mujoco
+
+import glfw
+from OpenGL import GL
+import OpenGL.GL.ARB.framebuffer_object
 
 class MuJoCoXr:
     def __init__(self):
-        # we use ContextObject for convenience
-        # (no need to manually create application, session...)
+        """
+        Things preventing us from using an xr.ContextObject instead of creating everything
+        ourselves:
+
+        - CtxObj creates one swapchain per eye. We need only one stereo swapchain, because
+          this is what the MuJoCo renderer is expecting.
+            - This is bypassable by creating a new swapchain ourselves.
+            - We also need to create a composition layer containing two views, one per eye.
+            - And to be able to use the "ContextObject.graphics" pre-made functions, we also
+              need another big layer with only one view containing both eyes side-by-side.
+        - CtxObj uses an internal OpenGLGraphics containing a lot of useful functions related
+          to rendering. However, this class initializes the OpenGL context using GLFW with some
+          hints to require context version 4.5 and Core Profile. MuJoCo is not compatible with
+          that: it requires OpenGL 3.1 or lower OR 3.2 and higher with the Compatibility Profile.
+            - This is NOT bypassable without recompiling all of pyopenxr
+        
+        We must therefore do everything ourselves, just like in vanilla OpenXR.
+
+        See the gl_example in the pyopenxr examples repository:
+        https://github.com/cmbruns/pyopenxr_examples/blob/main/xr_examples/gl_example.py
+        """
         self._context = xr.ContextObject(
             instance_create_info=xr.InstanceCreateInfo(
                 enabled_extension_names=[
@@ -106,6 +131,9 @@ class MuJoCoXr:
         model = mujoco.MjModel.from_xml_path(xml_path)
         data = mujoco.MjData(model)
 
+        gl_context = mujoco.glfw.GLContext(1000, 600)
+        gl_context.make_current()
+
         scene = mujoco.MjvScene(model, 1000)
         context = mujoco.MjrContext(model, mujoco.mjtFontScale.mjFONTSCALE_100)
 
@@ -118,6 +146,26 @@ class MuJoCoXr:
             mujoco.mjr_render(viewport, scene, context)
 
 if __name__ == "__main__":
+    if False:
+        if not glfw.init():
+            raise RuntimeError("GLFW initialization failed")
+        
+        glfw.window_hint(glfw.DOUBLEBUFFER, False)
+        # glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 4)
+        # glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 5)
+        # glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+        window = glfw.create_window(1000, 600, "gl_example", None, None)
+        if window is None:
+            raise RuntimeError("Failed to create GLFW window")
+        glfw.make_context_current(window)
+        
+        if not OpenGL.GL.ARB.framebuffer_object.glInitFramebufferObjectARB():
+            raise RuntimeError("No arb fbo")
+
+        for i in range(GL.glGetIntegerv(GL.GL_NUM_EXTENSIONS)):
+            extension_name = GL.glGetStringi(GL.GL_EXTENSIONS, i)
+            print("Supported OpenGL extension: ", extension_name)
+    
     with MuJoCoXr() as mjxr:
         """for frame, frame_state in enumerate(mjxr.loop()):
         GL.glClearColor(1, 0.7, 0.7, 1)  # pink
