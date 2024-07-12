674d03023707366eb1535d9b8cef05a903488570
SkytAsul <skytasul@gmail.com>
2024-07-12 13:02:06 +0200

    Convex hull decomposition

--- hand_script/hand_script.py
+++ hand_script/hand_script.py
@@ -3,6 +3,9 @@ from obj2mjcf import constants
 
 import trimesh
 from trimesh import Geometry, Scene
+import coacd
+
+import numpy as np
 
 from dm_control import mjcf
 from dm_control.mjcf import Element
@@ -11,7 +14,9 @@ from pathlib import Path
 import shutil
 import logging
 
+from dataclasses import dataclass
 import tyro
+from tyro.conf import Positional
 
 # All our assets have unique names so we do not need the hash in the filename
 class UnhashedAsset(mjcf.Asset):
@@ -31,18 +36,35 @@ def relative_pos(position: list[int], parent: Element):
         return relative_pos(new_position, parent.parent)
     else:
         return new_position
+
+@dataclass
+class Options:
+    hand_model: Positional[Path]
+    """Path to the hand model (.obj)"""
+    output: Positional[Path]
+    """Path to the output directory"""
+    overwrite: bool = False
+    """Should the output directory be automatically deleted (if present).
+    If not enabled, the user will be prompted.
+    """
+    reuse_old_assets: bool = True
+    """Should the existing .obj assets from another execution be reused.
+    Only effective if the user answered "Yes" to the overwrite prompt or if the overwrite option is enabled.
+    """
+    verbose: bool = True
+    """Should informations be printed to the standard output."""
     
 class HandCreator:
-    def __init__(self, output: Path):
-        self.output = output
+    def __init__(self, options: Options):
+        self.options = options
 
         logging.basicConfig()
 
-    def find_material_file(self, hand_model: Path) -> Path:
-        with open(hand_model, "r") as f:
+    def find_material_file(self) -> Path:
+        with open(self.options.hand_model, "r") as f:
             for line in f.readlines():
                 if line.startswith("mtllib"):
-                    mat_path = hand_model.parent / line[7:-1] # no \n at the end
+                    mat_path = self.options.hand_model.parent / line[7:-1] # no \n at the end
                     if mat_path.is_file():
                         logging.info(f"Found material file {mat_path}")
                         return mat_path
@@ -80,10 +102,10 @@ class HandCreator:
 
         return mtls
 
-    def load_meshes(self, hand_model: Path) -> Scene:
-        logging.info(f"Loading model file {hand_model}...")
+    def load_meshes(self) -> Scene:
+        logging.info(f"Loading model file {self.options.hand_model}...")
         mesh = trimesh.load_mesh(
-            hand_model,
+            self.options.hand_model,
             split_object=True,
             group_material=True,
             process=False,
@@ -95,7 +117,7 @@ class HandCreator:
         
         return mesh
 
-    def create_hand_part(self, fullname: str, parent: Element):
+    def create_hand_part(self, fullname: str, parent: Element, convex_parts: int = -1):
         logging.info(f"Creating hand part {fullname}...")
 
         geom = self.meshes.geometry.get(fullname)
@@ -112,29 +134,55 @@ class HandCreator:
         center = [ (max(axis) + min(axis))/2 for axis in [x,y,z] ]
         geom.vertices -= center
 
+        # Create the body in which we will put the geometries
+        partbody = parent.add("body", name=fullname, pos=relative_pos(center, parent))
+
+        # Visual mesh, geometry and material
+        visual_name = fullname + "_visual"
+        self.model.asset.add("material", name=material.name,
+                             specular=material.mjcf_specular(),
+                             shininess=material.mjcf_shininess(),
+                             rgba=material.mjcf_rgba())
         # We do not need to export the geometry to a file: by just getting the text contents,
         # we can pass them to MJCF via assets and it will take care of saving it.
         mesh_contents = geom.export(None, "obj", include_texture=True, header=None)
+        self.model.asset.add("mesh", name=visual_name,
+                             file=UnhashedAsset(mesh_contents, visual_name, ".obj"))
+        partbody.add("geom", mesh=visual_name, name=visual_name,
+                     dclass="visual", material=material.name)
 
-        # TODO decomposition
-        visual_name = fullname + "-visual"
-        collision_name = fullname + "-collision"
-        self.model.asset.add("mesh", file=UnhashedAsset(mesh_contents, visual_name, ".obj"), name=visual_name)
-        self.model.asset.add("material", name=material.name,
-                    specular=material.mjcf_specular(),
-                    shininess=material.mjcf_shininess(),
-                    rgba=material.mjcf_rgba())
+        # Collision meshes and geometries
+        if self.options.reuse_old_assets:
+            logging.info("Reusing convex hull from another time...")
+            for collision_file in self.options.output.glob(fullname + "_collision_*"):
+                i = collision_file.stem.split("_")[-1]
+                collision_name = collision_file.stem
+                self.model.asset.add("mesh", name=collision_name,
+                                    file=UnhashedAsset(collision_file.read_text(), collision_name, ".obj"))
+                colors = np.random.rand(3)
+                partbody.add("geom", mesh=collision_name, name=collision_name,
+                            dclass="collision", rgba = [*colors, 1])
+        else:
+            logging.info("Decomposing convex hull...")
+            mesh = coacd.Mesh(geom.vertices, geom.faces)
+            parts = coacd.run_coacd(mesh, max_convex_hull=convex_parts)
 
-        partbody = parent.add("body", name=fullname, pos=relative_pos(center, parent))
-        partbody.add("geom", mesh=visual_name, name=visual_name, dclass="visual", material=material.name)
-        partbody.add("geom", mesh=visual_name, name=collision_name, dclass="collision")
+            for i, (vs, fs) in enumerate(parts):
+                collision_name = f"{fullname}_collision_{i}"
+                part_geom = trimesh.Trimesh(vs, fs)
+                mesh_contents = part_geom.export(None, "obj", include_texture=False, header=None)
+                self.model.asset.add("mesh", name=collision_name,
+                                    file=UnhashedAsset(mesh_contents, collision_name, ".obj"))
+                colors = np.random.rand(3)
+                partbody.add("geom", mesh=collision_name, name=collision_name,
+                            dclass="collision", rgba = [*colors, 1])
 
         return partbody
 
     def create_finger(self, name: str, parent: Element, subparts: int):
         last_parent = parent
         for i in range(1, subparts+1):
-            last_parent = self.create_hand_part(f"{name}_{i}", last_parent)
+            last_parent = self.create_hand_part(f"{name}_{i}", last_parent, convex_parts=3)
 
     def add_defaults(self, model: mjcf.RootElement):
         visual = model.default.add("default", dclass="visual")
@@ -143,17 +191,24 @@ class HandCreator:
         collision = model.default.add("default", dclass="collision")
         collision.geom.set_attributes(type="mesh", group=3)
 
-    def main(self, hand_model_path: Path, overwrite = False):
-        if self.output.exists():
-            if not overwrite and input("Output already exists. Do you want to remove ? [Y/N] ").lower() != "y":
+    def main(self):
+        if self.options.verbose:
+            logging.getLogger().setLevel(logging.INFO)
+
+        should_remove = False
+        if self.options.output.exists():
+            if not self.options.overwrite and input("Output already exists. Do you want to remove ? [Y/N] ").lower() != "y":
                 return
-            shutil.rmtree(self.output)
+            
+            if self.options.reuse_old_assets:
+                should_remove = True
+            else:
+                shutil.rmtree(self.options.output)
+        else:
+            self.options.reuse_old_assets = False
 
-        # self.output.mkdir(parents=True)
-        # MJCF export takes care of that
-
-        self.materials = self.load_materials(self.find_material_file(hand_model_path))
-        self.meshes = self.load_meshes(hand_model_path)
+        self.materials = self.load_materials(self.find_material_file())
+        self.meshes = self.load_meshes()
 
         self.model = mjcf.RootElement(model="Hand")
 
@@ -165,13 +220,14 @@ class HandCreator:
         self.create_finger("middle", hand_body, 3)
         self.create_finger("annular", hand_body, 3)
         self.create_finger("pinky", hand_body, 3)
-        self.create_hand_part("wrist", hand_body)
-        self.create_hand_part("palm", hand_body)
+        self.create_hand_part("wrist", hand_body, convex_parts=6)
+        self.create_hand_part("palm", hand_body, convex_parts=7)
 
-        mjcf.export_with_assets(self.model, self.output, "Hand.xml", precision=3)
+        if should_remove:
+            shutil.rmtree(self.options.output)
 
-def generate_hand(hand_model: Path, output: Path, /, overwrite: bool = False):
-    HandCreator(output).main(hand_model, overwrite)
+        mjcf.export_with_assets(self.model, self.options.output, "Hand.xml", precision=3)
 
 if __name__ == "__main__":
-    tyro.cli(generate_hand)
+    options = tyro.cli(Options)
+    HandCreator(options).main()
